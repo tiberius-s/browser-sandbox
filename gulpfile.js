40a57b70f5dd764fd6c17008c4e0602bb50d8733
silivizzle <tiberius220@gmail.com>
2016-10-22 20:47:53 -0500

    tweaked the structure a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var bSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
 
-    browserSync.init({
-        server: "./app"
-    });
+  bSync.init({
+    server: "./app"
+  });
 
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/src/scss/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', bSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
+gulp.task('sass', function () {
+  return gulp.src("app/src/scss/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("app/css"))
+    .pipe(bSync.stream());
 });
 
 gulp.task('default', ['serve']);
