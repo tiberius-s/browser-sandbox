d92fea1b18194f6dfd399ed63f9b6904a42555b9
Tiberius Silivestru <tsilivestru@conversantmedia.com>
2016-10-24 18:10:16 -0500

    added some support for adding js libraries

---
+++ app/src/js/app.js
@@ -0,0 +1,10 @@
+/* This is the entry point for the application.Since we are using Browserify,
+you will be able to add libraries here with a require() function.
+
+Example:
+
+require('angular');                   //  include the AngularJS library
+
+var app = angular.module('app', []);  //  Angular code as usual.
+
+*/
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,51 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var bSync = require('browser-sync').create();
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const watchify = require('watchify');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function () {
-
-  bSync.init({
-    server: "./app"
-  });
-
-  gulp.watch("app/src/scss/*.scss", ['sass']);
-  gulp.watch("app/*.html").on('change', bSync.reload);
+// initialize the variables needed
+const bundler = browserify({
+    entries: ['./app/src/js/app.js'],
+    debug: true
 });
+const watcher = watchify(bundler);
+
+// bundle the code
+function bundleApp () {
+    return bundler.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./app/js'))
+        .pipe(browserSync.stream({once: true}));
+}
+
+//update the bundle on change
+watcher.on('update', bundleApp);
+
+// bundle the main app
+gulp.task('bundle', bundleApp);
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-  return gulp.src("app/src/scss/*.scss")
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("app/css"))
-    .pipe(bSync.stream());
+    return gulp.src("app/src/scss/*.scss")
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'bundle'], function() {
+
+    browserSync.init({
+        server: "./app",
+        routes: {
+            './node_modules': 'node_modules'
+        }
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
